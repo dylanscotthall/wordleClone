9c32fdd2e62771810107a90659a29a0d429cef82
dylanscotthall <dylanscotthall@gmail.com>
2023-02-28 06:13:06 +0000

    added text input and output

--- app/src/main/java/com/gumballdylan/wordleclone/MainActivity.kt
+++ app/src/main/java/com/gumballdylan/wordleclone/MainActivity.kt
@@ -2,10 +2,19 @@ package com.gumballdylan.wordleclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun updateName(view: View) {
+        val textView = findViewById<TextView>(R.id.txt)
+        val inputText = findViewById<EditText>(R.id.textInput)
+        textView.text = "Hello " + inputText.text
     }
 }
