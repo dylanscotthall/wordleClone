3202b5a889a2a4f09144becda0175f3c7e84c3a1
HumanClone <Pranhav1507@gmail.com>
2023-02-28 07:43:12 +0000

    basic funtions and logic plan in comments

--- app/src/main/java/com/gumballdylan/wordleclone/MainActivity.kt
+++ app/src/main/java/com/gumballdylan/wordleclone/MainActivity.kt
@@ -17,4 +17,54 @@ class MainActivity : AppCompatActivity() {
         val username = findViewById<TextView>(R.id.textUsername)
         username.setText(str)
     }
+
+    //words to use
+    val words=arrayOf<String>("SNARE", "YUPPY","TEARS", "QUEST", "PIANO", "CREAM", "ROATE","TRAIN")
+    val wordsAm:Int=7
+    var length=0
+    var word=-1
+    var letters=  mutableListOf<String>()
+    //randomly selects which word
+    fun getWord():String
+    {
+        word=(0..wordsAm).random()
+        length=words[word].length
+        return words[word]
+    }
+
+
+
+
+    //checks if the letter is in the word and if it has already been used
+    fun checkLetter(c:String,word:String ):Boolean
+    {
+        if (letters.contains(c))
+        {
+            //use on ui for already used letter
+            return false
+        }
+        else
+        {
+            letters.add(c)
+            return word.contains(c,ignoreCase = true)
+        }
+    }
+
+    fun fullLogic()
+    {
+        //get word
+        //display word length but hidden letters  UI
+        //allow user input  UI
+        //check if letter is in word
+        //add letter to list of used letters
+        //display correct or incorrect  UI
+        //show letter block  UI
+        // and then show word at end UI
+        //allow a reset and retry
+
+    }
+
+
+
 }
+
