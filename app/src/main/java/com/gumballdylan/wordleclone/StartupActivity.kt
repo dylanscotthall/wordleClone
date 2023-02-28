2434e8407fe5d48855490f1297b336dea84038b6
dylanscotthall <dylanscotthall@gmail.com>
2023-02-28 07:42:20 +0000

    recieves users name and send it to new activity

--- app/src/main/java/com/gumballdylan/wordleclone/StartupActivity.kt
+++ app/src/main/java/com/gumballdylan/wordleclone/StartupActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -15,11 +16,19 @@ class StartupActivity : AppCompatActivity() {
     }
 
     fun updateName(view: View) {
+        //receive name from input text and displays it in text view
         val textView = findViewById<TextView>(R.id.txt)
         val inputText = findViewById<EditText>(R.id.textInput)
         textView.text = "Hello " + inputText.text
+
+        //toast message to show name inputed
+        val toast = Toast.makeText(applicationContext, textView.text, Toast.LENGTH_LONG)
+        toast.setMargin(50f, 50f)
+        toast.show()
+
+        // intent to go to mainActivity for our game
         val i = Intent(this@StartupActivity, MainActivity::class.java)
-        i.putExtra("username", inputText.text.toString());
+        i.putExtra("username", inputText.text.toString())
         startActivity(i)
     }
 }
