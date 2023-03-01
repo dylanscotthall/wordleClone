1fe2e1752a63d28fde2faf680fa0a791bc42f0ff
dylanscotthall <dylanscotthall@gmail.com>
2023-03-01 07:49:49 +0000

    updated some stuff and implemented gumballs logic

--- app/src/main/java/com/gumballdylan/wordleclone/MainActivity.kt
+++ app/src/main/java/com/gumballdylan/wordleclone/MainActivity.kt
@@ -2,7 +2,10 @@ package com.gumballdylan.wordleclone
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -11,29 +14,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setWord()
+
+        //show placeholders as _
+        var placeholders = ""
+        for (c in randWord){
+            placeholders += "_ "
+        }
+
+        findViewById<TextView>(R.id.placeholderText).text = placeholders
         val intent = intent
-        val str = intent.getStringExtra("username").toString()
-        Log.d("str", str)
-        val username = findViewById<TextView>(R.id.textUsername)
-        username.setText(str)
+        val greetMsg = "Hello " + intent.getStringExtra("username").toString() + " guess the word!"
+        val username = findViewById<TextView>(R.id.greetText)
+        username.text = greetMsg
+    }
+
+    fun wordCheck(view: View) {
+        val guess = findViewById<EditText>(R.id.wordInput).text
+        findViewById<TextView>(R.id.guessCorrect).text = guesses.toString()
+        fullLogic(guess.toString())
     }
 
     //words to use
-    val words=arrayOf<String>("SNARE", "YUPPY","TEARS", "QUEST", "PIANO", "CREAM", "ROATE","TRAIN")
-    val wordsAm:Int=7
-    val length:Int =5
-    var word=-1
-    var randWord=""
-    var guesses=0;
+    val words =
+        arrayOf<String>("SNARE", "YUPPY", "TEARS", "QUEST", "PIANO", "CREAM", "ROATE", "TRAIN")
+    val wordsAm: Int = words.size-1
+    val length: Int = 5
+    var word = -1
+    var randWord = ""
+    var guesses = 0
+    var corLetters = mutableListOf<Char>()
+
 
     //randomly selects which word
-    fun getWord():String
-    {
-        word=(0..wordsAm).random()
+    fun getWord(): String {
+        word = (0..wordsAm).random()
         return words[word]
     }
-
-
 
 
     //checks if the letter is in the word and if it has already been used
@@ -51,71 +68,65 @@ class MainActivity : AppCompatActivity() {
         }
     }*/
 
-    fun setWord()
-    {
-        randWord=getWord()
+    fun setWord() {
+        randWord = getWord()
     }
 
-    fun fullLogic(iWord:String,)
-    {
-       guesses+1
-        if (iWord.equals(randWord))
-        {
-            //display correct word output or whatever
-        }
-        else
-        {
+    fun fullLogic(iWord: String) {
+        guesses += 1
+        if (iWord.equals(randWord)) {
+            Toast.makeText(this, "YOU WIN", Toast.LENGTH_LONG).show()
+        } else {
 
-            var corLetters = mutableListOf<Char>()
-
-            for (i in 0..4)
-            {
-
-                var letter = iWord.get(i)
-                for (j in 0..4)
-                {
-                    var wletter = randWord.get(j)
-                    if (letter==wletter)
-                    {
-                        if (!(corLetters.contains(letter)))
-                        {
-                            corLetters.add(letter)
-                        }
+            //slightly improved efficiency only using one for loop
+            for (c in iWord) {
+                if (randWord.contains(c.uppercaseChar())) {
+                    if (!corLetters.contains(c.uppercaseChar())) {
+                        corLetters.add(c.uppercaseChar())
                     }
                 }
             }
+//            for (i in 0..4)
+//            {
+//
+//                var letter = iWord.get(i)
+//                for (j in 0..4)
+//                {
+//                    var wletter = randWord.get(j)
+//                    if (letter==wletter)
+//                    {
+//                        if (!(corLetters.contains(letter)))
+//                        {
+//                            corLetters.add(letter)
+//                        }
+//                    }
+//                }
+//            }
 
-            if (corLetters.isNotEmpty())
-            {
-                //return the letters from corletters and say they are correct
-                if (corLetters.count()==5)
-                {
+            if (corLetters.isNotEmpty()) {
+                val corLett = corLetters.toString()
+                findViewById<TextView>(R.id.currentCorLetters).text = corLett
+                if (corLetters.count() == 5) {
                     //right letters but wrong order
                 }
+            } else {
+                findViewById<TextView>(R.id.currentCorLetters).text = "no correct letters yet"
             }
-            else
-            {
-                //output for incorrect letters
-            }
-            if (guesses==4)
-            {
-                //display that it is the last guess
-
+            if (guesses == 4) {
+                Toast.makeText(this, "Last Chance", Toast.LENGTH_LONG).show()
             }
 
-            if ( guesses==5)
-            {
-                //lock out user and end screen
+            if (guesses == 5) {
+                reset()
             }
         }
 
 
     }
 
-    fun reset()
-    {
-        guesses=0
-        randWord=getWord();
+    fun reset() {
+        guesses = 0
+        randWord = getWord();
     }
 
 
